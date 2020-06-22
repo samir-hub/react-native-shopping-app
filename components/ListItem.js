e65b63bc5c1ecc313a7ffd456d66e1074602e3e2
Samir Lilienfeld <samirlilienfeld@hotmail.com>
2020-06-22 19:47:37 -0400

    ListItems displaying items!

--- components/ListItem.js
+++ components/ListItem.js
@@ -5,14 +5,27 @@ const ListItem = ({ item }) => {
   return (
     <TouchableOpacity style={styles.listItem}>
       <View style={styles.listItemView}>
-        <Text style={styles.listItemText}></Text>
+        <Text style={styles.listItemText}>{item.text}</Text>
       </View>
     </TouchableOpacity>
   );
 };
 
 const styles = StyleSheet.create({
-  listItem: {},
+  listItem: {
+      padding: 15,
+      backgroundColor: '#f8f8f8',
+      borderBottomWidth: 1,
+      borderColor: '#eee'
+  },
+  listItemView: {
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      alignItems: 'center'
+  },
+  listItemText: {
+      fontSize: 18
+  }
 });
 
 export default ListItem;
