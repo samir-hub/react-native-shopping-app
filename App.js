9747e42776b68f40cd8e06a993d3691c35cc1d4a
Samir Lilienfeld <samirlilienfeld@hotmail.com>
2020-06-28 18:52:09 -0400

    home screen set up

--- App.js
+++ App.js
@@ -1,80 +1,20 @@
 import "react-native-gesture-handler";
-import React, { useState } from "react";
-import { StyleSheet, FlatList, View, Alert } from "react-native";
+import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from '@react-navigation/stack';
-import Header from "./components/Header";
-import ListItem from "./components/ListItem";
-import AddItem from "./components/AddItem";
+import { createStackNavigator } from "@react-navigation/stack";
+import HomeScreen from "./components/HomeScreen";
+//import Header from "./components/Header";
 
 const Stack = createStackNavigator();
 
 const App = () => {
-  const genId = () => {
-    return Math.floor(Math.random() * 10000000);
-  };
-
-  const [items, setItems] = useState([
-    {
-      id: genId(),
-      text: "Milk",
-    },
-    {
-      id: genId(),
-      text: "Cheese",
-    },
-    {
-      id: genId(),
-      text: "Eggs",
-    },
-    {
-      id: genId(),
-      text: "Juice",
-    },
-  ]);
-
-  const deleteItem = (id) => {
-    setItems((prevItems) => {
-      return prevItems.filter((item) => item.id != id);
-    });
-  };
-
-  const addItem = (item) => {
-    let found = items.find((element) => element.text === item);
-
-    if (!item) {
-      Alert.alert("Error", "Please enter an item", { text: "Ok" });
-    } else if (found) {
-      Alert.alert("Error", "Item already exists", { text: "Ok" });
-    } else {
-      setItems((prevItems) => {
-        return [...prevItems, { id: genId(), text: item }];
-      });
-    }
-  };
-
   return (
     <NavigationContainer>
-    
-      <View style={styles.container}>
-        <Header title="Shopping List" />
-        <AddItem addItem={addItem} />
-        <FlatList
-          data={items}
-          renderItem={({ item }) => (
-            <ListItem item={item} deleteItem={deleteItem} />
-          )}
-        />
-      </View>
-      
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
     </NavigationContainer>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-});
 
 export default App;
---
+++ components/HomeScreen.js
@@ -0,0 +1,69 @@
+import React, { useState } from "react";
+import { StyleSheet, FlatList, View, Alert } from "react-native";
+import ListItem from "./ListItem";
+import AddItem from "./AddItem";
+
+const HomeScreen = () => {
+  const genId = () => {
+    return Math.floor(Math.random() * 10000000);
+  };
+
+  const [items, setItems] = useState([
+    {
+      id: genId(),
+      text: "Milk",
+    },
+    {
+      id: genId(),
+      text: "Cheese",
+    },
+    {
+      id: genId(),
+      text: "Eggs",
+    },
+    {
+      id: genId(),
+      text: "Juice",
+    },
+  ]);
+
+  const deleteItem = (id) => {
+    setItems((prevItems) => {
+      return prevItems.filter((item) => item.id != id);
+    });
+  };
+
+  const addItem = (item) => {
+    let found = items.find((element) => element.text === item);
+
+    if (!item) {
+      Alert.alert("Error", "Please enter an item", { text: "Ok" });
+    } else if (found) {
+      Alert.alert("Error", "Item already exists", { text: "Ok" });
+    } else {
+      setItems((prevItems) => {
+        return [...prevItems, { id: genId(), text: item }];
+      });
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      <AddItem addItem={addItem} />
+      <FlatList
+        data={items}
+        renderItem={({ item }) => (
+          <ListItem item={item} deleteItem={deleteItem} />
+        )}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
+
+export default HomeScreen;
