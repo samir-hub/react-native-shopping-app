2bbda61cd946d988d8571a3d4c54bef8b27aedc6
Samir Lilienfeld <samirlilienfeld@hotmail.com>
2020-06-28 19:07:52 -0400

    added initialRoute

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import HomeScreen from "./components/HomeScreen";
+import DetailsScreen from "./components/DetailsScreen"; 
 //import Header from "./components/Header";
 
 const Stack = createStackNavigator();
@@ -10,8 +11,9 @@ const Stack = createStackNavigator();
 const App = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator initialRouteName="Home">
         <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Details" component={DetailsScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
